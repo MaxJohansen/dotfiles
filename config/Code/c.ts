0a0e2f03cff6b8f6b36c9a289497099296ed5a30
Mads Johansen <johansen.max@gmail.com>
2019-08-22 01:25:13 +0200

    Add C/C++ tools VSCode package

--- config/Code/c.ts
+++ config/Code/c.ts
@@ -1,6 +1,7 @@
 import { usePackage } from "vscode-use-package";
 
 export function init() {
+  usePackage("ms-vscode.cpptools");
   usePackage("llvm-vs-code-extensions.vscode-clangd");
   usePackage("openningia.vscode-google-test-adapter");
 }
